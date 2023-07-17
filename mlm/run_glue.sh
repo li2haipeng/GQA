a6a16e0db995ac7301d17a3e7b92382bfc5c06fd
li2haipeng <li2haipeng@gmail.com>
2023-07-17 18:47:03 +0000

    init

--- mlm/run_glue.sh
+++
@@ -1,9 +0,0 @@
-export TASK_NAME=mrpc
-
-python3 run_glue.py \
-  --model_name_or_path /home/ubuntu/mlm/mqa_roberta_test \
-  --tokenizer_name roberta-base \
-  --task_name $TASK_NAME \
-  --do_eval \
-  --max_seq_length 512 \
-  --output_dir /tmp/$TASK_NAME/
